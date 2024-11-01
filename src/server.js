9ebf5e0ebc096c71367873c9e41bab4f880ff653
Cuonggkw <cp9188950@gmail.com>
2024-11-01 11:15:49 +0700

    up-10

--- src/server.js
+++ src/server.js
@@ -9,8 +9,11 @@ const cors = require("cors");
 let app = express();
 app.use(cors());
 
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+// app.use(bodyParser.json({ limit: "50mb" }));
+// app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 apiWebRouter(app);
 
