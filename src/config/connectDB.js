bea8a6e44861c81a0a01d233e3a0ce52d2494a44
Cuonggkw <cp9188950@gmail.com>
2024-11-01 22:39:13 +0700

    up-28

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -3,11 +3,11 @@ const { Sequelize } = require("sequelize");
 
 // Option 3: Passing parameters separately (other dialects)
 const sequelize = new Sequelize("restaurant", "root", null, {
-  host: "localhost",
+  host: "127.0.0.1",
   dialect: "mysql",
   // dialectModule: require("mysql2"),
   logging: false,
-  port: 3306,
+  port: "3306",
   dialectOptions: {
     connectTimeout: 60000, // 60 giây
   },
