cf785b998a1dcb33a08de117f6fd007b7464305f
Cuonggkw <cp9188950@gmail.com>
2024-11-01 17:29:39 +0700

    up-22

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize("restaurant", "root", null, {
   host: "localhost",
   dialect: "mysql",
   logging: false,
-  port: process.env.PORT || 3306,
+  port: process.env.PORT || 3000,
   dialectOptions: {
     connectTimeout: 60000, // 60 giây
   },
